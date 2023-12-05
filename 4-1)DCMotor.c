43f59bfad62e378335cc8a7c12bde40b4cddb607
YUNSUNGWOONG <100409315+YUNSUNGWOONG@users.noreply.github.com>
2023-12-05 10:30:02 +0900

    Update 4-1)DCMotor.c

--- 4-1)DCMotor.c
+++ 4-1)DCMotor.c
@@ -11,73 +11,37 @@ void LPIT0_init (uint32_t delay)
 {
    uint32_t timeout;
 
-	/*!
-	    * LPIT Clocking:
-	    * ==============================
-	    */
-	  PCC->PCCn[PCC_LPIT_INDEX] = PCC_PCCn_PCS(6);    /* Clock Src = 6 (SPLL2_DIV2_CLK)*/
-	  PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_CGC_MASK; /* Enable clk to LPIT0 regs       */
+	  PCC->PCCn[PCC_LPIT_INDEX] = PCC_PCCn_PCS(6);    
+	  PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_CGC_MASK; 
 
-	  /*!
-	   * LPIT Initialization:
-	   */
-	  LPIT0->MCR |= LPIT_MCR_M_CEN_MASK;  /* DBG_EN-0: Timer chans stop in Debug mode */
-	                                        /* DOZE_EN=0: Timer chans are stopped in DOZE mode */
-	                                        /* SW_RST=0: SW reset does not reset timer chans, regs */
-	                                        /* M_CEN=1: enable module clk (allows writing other LPIT0 regs) */
+	  LPIT0->MCR |= LPIT_MCR_M_CEN_MASK;  
 
   timeout=delay* 40;
-  LPIT0->TMR[0].TVAL = timeout;      /* Chan 0 Timeout period: 40M clocks */
+  LPIT0->TMR[0].TVAL = timeout;     
   LPIT0->TMR[0].TCTRL |= LPIT_TMR_TCTRL_T_EN_MASK;
-                                     /* T_EN=1: Timer channel is enabled */
-                              /* CHAIN=0: channel chaining is disabled */
-                              /* MODE=0: 32 periodic counter mode */
-                              /* TSOT=0: Timer decrements immediately based on restart */
-                              /* TSOI=0: Timer does not stop after timeout */
-                              /* TROT=0 Timer will not reload on trigger */
-                              /* TRG_SRC=0: External trigger soruce */
-                              /* TRG_SEL=0: Timer chan 0 trigger source is selected*/
+                                    
 }
 
 void delay_us (volatile int us){
-   LPIT0_init(us);           /* Initialize PIT0 for 1 second timeout  */
-   while (0 == (LPIT0->MSR & LPIT_MSR_TIF0_MASK)) {} /* Wait for LPIT0 CH0 Flag */
-               lpit0_ch0_flag_counter++;         /* Increment LPIT0 timeout counter */
-               LPIT0->MSR |= LPIT_MSR_TIF0_MASK; /* Clear LPIT0 timer flag 0 */
+   LPIT0_init(us);           
+   while (0 == (LPIT0->MSR & LPIT_MSR_TIF0_MASK)) {} 
+               lpit0_ch0_flag_counter++;         
+               LPIT0->MSR |= LPIT_MSR_TIF0_MASK; 
 }
 
 void FTM_init (void){
-
-	//FTM0 clocking
-	PCC->PCCn[PCC_FTM0_INDEX] &= ~PCC_PCCn_CGC_MASK;		//Ensure clk diabled for config
-	PCC->PCCn[PCC_FTM0_INDEX] |= PCC_PCCn_PCS(0b010)		//Clocksrc=1, 8MHz SIRCDIV1_CLK
-								| PCC_PCCn_CGC_MASK;		//Enable clock for FTM regs
-
-//FTM0 Initialization
-	FTM0->SC = FTM_SC_PWMEN2_MASK							//Enable PWM channel 1output
+	PCC->PCCn[PCC_FTM0_INDEX] &= ~PCC_PCCn_CGC_MASK;		
+	PCC->PCCn[PCC_FTM0_INDEX] |= PCC_PCCn_PCS(0b010)		
+								| PCC_PCCn_CGC_MASK;	
+	FTM0->SC = FTM_SC_PWMEN2_MASK							
 			  |FTM_SC_PWMEN3_MASK
-				|FTM_SC_PS(0.5);								//TOIE(timer overflow Interrupt Ena) = 0 (deafault)
-															//CPWMS(Center aligned PWM Select) =0 (default, up count)
-															/* CLKS (Clock source) = 0 (default, no clock; FTM disabled) 	*/
-															/* PS (Prescaler factor) = 1. Prescaler = 2 					*/
-
-	FTM0->MOD = 8000-1;									//FTM0 counter final value (used for PWM mode)
-															// FTM0 Period = MOD-CNTIN+0x0001~=16000 ctr clks=8ms
-															//8Mhz /2 =4MHz
+				|FTM_SC_PS(0.5);								
+	FTM0->MOD = 8000-1;								
 	FTM0->CNTIN = FTM_CNTIN_INIT(0);
-
-
 	FTM0->CONTROLS[2].CnSC |=FTM_CnSC_MSB_MASK;
-	FTM0->CONTROLS[2].CnSC |=FTM_CnSC_ELSA_MASK;			/* FTM0 ch1: edge-aligned PWM, low true pulses 		*/
-															/* CHIE (Chan Interrupt Ena) = 0 (default) 			*/
-															/* MSB:MSA (chan Mode Select)=0b10, Edge Align PWM		*/
-															/* ELSB:ELSA (chan Edge/Level Select)=0b10, low true 	*/
-
+	FTM0->CONTROLS[2].CnSC |=FTM_CnSC_ELSA_MASK;			
 	FTM0->CONTROLS[3].CnSC |=FTM_CnSC_MSB_MASK;
 	FTM0->CONTROLS[3].CnSC |=FTM_CnSC_ELSA_MASK;
-
-
-	//start FTM0 counter with clk source = external clock (SOSCDIV1_CLK)
 	FTM0->SC|=FTM_SC_CLKS(3);
 
 }
@@ -86,18 +50,9 @@ void FTM_init (void){
  void PORT_init (void)
 {
 
-	 /*
-	 * Pins definitions
-	 * ===================================================
-	 *
-	 * Pin number        | Function
-	 * ----------------- |------------------
-	 * PTD0		     	 | FTM0CH2
-	 * PTD1			     | FTM0CH3
-	 */
-  PCC->PCCn[PCC_PORTD_INDEX ]|=PCC_PCCn_CGC_MASK;   /* Enable clock for PORTD */
-  PORTD->PCR[0]|=PORT_PCR_MUX(2);           		/* Port D16: MUX = ALT2, FTM0CH1 */
-  PORTD->PCR[1]|=PORT_PCR_MUX(2);           		/* Port D16: MUX = ALT2, FTM0CH3 */
+  PCC->PCCn[PCC_PORTD_INDEX ]|=PCC_PCCn_CGC_MASK;   
+  PORTD->PCR[0]|=PORT_PCR_MUX(2);           		
+  PORTD->PCR[1]|=PORT_PCR_MUX(2);           		
 }
 
 
@@ -105,25 +60,22 @@ void FTM_init (void){
 
 int main(void)
 {
-  uint32_t adcResultInMv=0;	/*< ADC0 Result in miliVolts */
+  uint32_t adcResultInMv=0;
   int D=0;
-  SOSC_init_8MHz();      /* Initialize system oscillator for 8 MHz xtal */
-  SPLL_init_160MHz();    /* Initialize SPLL to 160 MHz with 8 MHz SOSC */
-  NormalRUNmode_80MHz(); /* Init clocks: 80 MHz SPLL & core, 40 MHz bus, 20 MHz flash */
+  SOSC_init_8MHz();      
+  SPLL_init_160MHz();   
+  NormalRUNmode_80MHz(); 
   FTM_init();
-  ADC_init();            /* Init ADC resolution 12 bit			*/
-  PORT_init();           /* Configure ports */
+  ADC_init();            
+  PORT_init();           
 	  for(;;)
 	  {
-    	  convertAdcChan(13);                   /* Convert Channel AD12 to pot on EVB 	*/
-    	  while(adc_complete()==0){}            /* Wait for conversion complete flag 	*/
-    	  adcResultInMv = read_adc_chx();       /* Get channel's conversion results in mv */
-
-
+    	  convertAdcChan(13);                   
+    	  while(adc_complete()==0){}            
+    	  adcResultInMv = read_adc_chx();      
 
     	  D=adcResultInMv*3.0;
 
-    	  /* Combine mode and dead-time enable for channel2 and channel3 */
     	  FTM0->COMBINE |= FTM_COMBINE_SYNCEN1_MASK | FTM_COMBINE_COMP1_MASK | FTM_COMBINE_DTEN1_MASK;
 
     	  FTM0->CONTROLS[2].CnV=FTM_CnV_VAL(D); //8000~0 duty; ex(7200=> Duty 0.1 / 800=>Duty 0.9)
